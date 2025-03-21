c7395130b02ba3305241211947cc86417597ee89
SivaChandrasekharJavvadi <sivachandrasekharj.3@gmail.com>
2025-03-21 12:55:24 +0530

    Updated the files

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "ami" {
+  default = "ami-0b0ea68c435eb488d"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "instance_name" {
+  default = ""
+}
--- variables.tfvars
+++
@@ -1,5 +0,0 @@
-  ami = "t2.micro"
-
-  instance_type = "ami-0b0ea68c435eb488d"
-
-  instance_name = ""
